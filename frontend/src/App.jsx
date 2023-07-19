614a4b48797f779ec235d75b4a426db0ae01914a
AikoFr <aikootv@gmail.com>
2023-07-19 16:56:11 +0200

    Configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import Home from "./pages/Home";
-
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin/Signin";
+import Signup from "./pages/Signin/Signup";
+import Error from "./pages/Error";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
