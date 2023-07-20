9500f5b46f7819fa6358f40962190155d5ac41bb
AikoFr <aikootv@gmail.com>
2023-07-20 14:10:47 +0200

    Ajout de la navbar + scss

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BsCarFront, BsBook } from "react-icons/bs";
+import { useCurrentUser } from "../../contexts/UserContext";
+import logoLspd from "../../assets/logo_lspd.png";
+import "./NavBar.scss";
+
+function NavBar() {
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+  const { setUser, user } = useCurrentUser();
+
+  const openNav = () => {
+    setIsSideNavOpen(true);
+  };
+
+  const closeNav = () => {
+    setIsSideNavOpen(false);
+  };
+
+  const logout = () => {
+    setUser("");
+  };
+
+  return (
+    <div>
+      <div className={`sidenav ${isSideNavOpen ? "active" : ""}`}>
+        <button type="button" className="close" onClick={closeNav}>
+          X
+        </button>
+
+        <ul>
+          <li>
+            {user.role === "lspd" && (
+              <>
+                <img
+                  className="logo-lspd"
+                  src={logoLspd}
+                  alt="Insignes de la Los Santos Police Department"
+                />
+                <h2 className="menu-info">
+                  {user.firstname} {user.lastname}
+                </h2>
+                <Link to="/" className="menu-item" onClick={closeNav}>
+                  <AiOutlineHome /> Home
+                </Link>
+                <Link to="/citizens" className="menu-item" onClick={closeNav}>
+                  <AiOutlineUser /> Citoyens
+                </Link>
+                <Link to="/vehicles" className="menu-item" onClick={closeNav}>
+                  <BsCarFront /> Véhicules
+                </Link>
+                <Link to="/fines" className="menu-item" onClick={closeNav}>
+                  <BsBook /> Amendes
+                </Link>
+              </>
+            )}
+
+            <button type="button" className="deconnexion" onClick={logout}>
+              Déconnexion
+            </button>
+          </li>
+        </ul>
+      </div>
+
+      <button type="button" id="openBtn" onClick={openNav}>
+        <span className="burger-icon">
+          <span />
+          <span />
+          <span />
+        </span>
+      </button>
+    </div>
+  );
+}
+export default NavBar;
--- frontend/src/layout/LspdLayout.jsx
+++ frontend/src/layout/LspdLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useCurrentUser } from "../contexts/UserContext";
+import NavBar from "../components/NavBar/NavBar";
 
 function LspdLayout() {
   const { user } = useCurrentUser();
@@ -8,6 +9,7 @@ function LspdLayout() {
   if (user.role === "lspd") {
     return (
       <div>
+        <NavBar />
         <Outlet />
       </div>
     );
